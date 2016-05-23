8dbaf8cb4a17f525b7dd331006edba2b83e0ba3f
Arunfrugal <arun@frugaltalent.com>
2016-05-23 14:24:28 +0530

    master Dashboard design update

--- public/angular/controller/MasterManagement/masterdashCtrl.js
+++ public/angular/controller/MasterManagement/masterdashCtrl.js
@@ -1,4 +1,30 @@
-var app=angular.module("app");
-app.controller("masterDashCtrl",function($scope){
-console.log("Master Dashboard");
+
+var app=angular.module('app')
+app.controller('masterDashCtrl', function($scope) {
+ console.log("masterDashCtrl");
+
+//get the window width
+var windowwidth = $(window).width();
+//while window width >960 apply the style--while load the window
+if (windowwidth>960) {
+$scope.widthEle=5;
+}else{
+$scope.widthEle=1;
+}
+//Resize window event
+$(window).resize(function(){
+if($(this).width() != windowwidth){
+windowwidth = $(this).width();
+console.log(windowwidth);
+//while window width >960 apply the style--while shrink the window
+if (windowwidth>960) {
+$scope.widthEle=5;
+}else{
+$scope.widthEle=1;
+}
+}
+});
+
+
 })
+  
