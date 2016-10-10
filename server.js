2100edde3403a7781d1e24b1db22137a56d62170
rajeev ranjan <becoolrajeev@gmail.com>
2016-10-11 00:38:49 +0530

    rip-added

--- server.js
+++ server.js
@@ -141,3 +141,14 @@ app.post('/glowfanoff', function(req, res)
   console.log(req.body);
 });
 
+var gpio = require('rpi-gpio');
+ 
+gpio.setup(7, gpio.DIR_OUT, write);
+ 
+function write() {
+    gpio.write(7, true, function(err) {
+        if (err) throw err;
+        console.log('Written to pin');
+    });
+}
+
